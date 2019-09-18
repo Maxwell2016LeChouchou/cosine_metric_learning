201c99c2054366ca988bce700c2d0e4d2d7f44c1
maxwell <jwang212@uottawa.ca>
2019-09-18 15:33:02 -0400

    continue for train_ytfaces.py file

--- train_ytfaces.py
+++ train_ytfaces.py
@@ -14,10 +14,36 @@ class youtube_faces(object):
         self._seed = seed
     
     def read_train(self):
-        image_list, yt_person_name, yt_dir_file = youtube_faces.read_train_split_to_str(self._dataset_dir)
-        train
+        image_list, yt_person_name, yt_dir_file = youtube_faces.read_train_split_to_str(
+            self._dataset_dir)
+        train_indices, _ = util.create_validation_split(
+            np.asarray(ids, np.int64), self._num_validation_y, self._seed)
+        
+        image_list = [image_list[i] for i in train_indices]
+        yt_person_name = [yt_person_name[i] for i in train_indices]
+        yt_dir_file = [yt_dir_file[i] for i in train_indices]
+
+        return image_list, yt_person_name, yt_dir_file
+    
+    def read_validation(self):
+        image_list, yt_person_name, yt_dir_file = youtube_faces.read_train_split_to_str(
+            self._dataset_dir)
+        _, valid_indices = util.create_validation_split(
+            np.asarray(ids, np.int64), self._num_validation_y, self._seed)
+
+        image_list = [image_list[i] for i in valid_indices]
+        yt_person_name = [yt_person_name[i] for i in valid_indices]
+        yt_dir_file = [yt_dir_file[i] for i in valid_indices]
+
+        return image_list, yt_person_name, yt_dir_file
+
+    def read_test(self):
+        return youtube_faces.read_test_split_to_str(self._dataset_dir)
+
+def main():
 
 
+        
 
         
 
